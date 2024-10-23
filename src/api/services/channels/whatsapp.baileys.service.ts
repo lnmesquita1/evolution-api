dfeeee978beb2c31f19e590ef5717fd1086bba9f
Lucas Nogueira Mesquita <amcom_lucas@badesc.gov.br>
2024-10-23 09:57:28 -0300

    Change web version

--- src/api/services/channels/whatsapp.baileys.service.ts
+++ src/api/services/channels/whatsapp.baileys.service.ts
@@ -588,7 +588,7 @@ export class BaileysStartupService extends ChannelStartupService {
     const integrationData = await this.repository.integration.find(this.instanceName);
     const webVersion = integrationData.webVersion 
         ? integrationData.webVersion.split('.').map(Number) 
-        : [2, 3000, 1015901307]
+        : [2, 3000, 1017381166]
 
     const socketConfig: UserFacingSocketConfig = {
       ...options,
